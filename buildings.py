0495c05e17204a44c004263489ace6e55ac3e78a
David <lordmordevil@gmail.com>
2013-01-28 23:03:06 +0200

    Stuff is broken
    but the new foundation will make adding stuff easyer

--- buildings.py
+++ buildings.py
@@ -1,58 +1,5 @@
-from pygamehelper import *
-from pygame import *
-from pygame.locals import *
-from vec2d import *
-from random import uniform
-
-def load_map(buildings):
-    map_file = open('assets/map.csv', 'r')
-
-    map_line = map_file.readline()
-
-    coords = [0, 0]
-
-    while not map_line == '':
-        cells = map_line.split(";")
-        for cell in cells:
-            cell_info = cell.split("/")
-            if not cell_info[0] == "0":
-                if cell_info[1] == "0/n":
-                    cell_info[1] = "0"
-                buildings.walls.append(Wall(vec2d(coords[0] * 30 + 15, coords[1] * 30 + 15), cell_info[0], cell_info[1]))
-            coords[0] += 1
 
 
-        coords[1] += 1
-        coords[0] = 0
-        map_line = map_file.readline()
 
 
-class Wall():
 
-    sprite_dict = { '1':"wall", '2':"door", '3':"window"}
-
-    size = 25
-
-    def __init__(self, pos, wall_type, direct):    
-        self.pos = pos
-        self.dir = direct
-        self.type = wall_type
-
-        self.sprite = pygame.image.load("assets/obsticles/" + self.sprite_dict[wall_type] + ".png")
-        if not direct == "0":
-        	self.sprite = pygame.transform.rotate(self.sprite, int( 90 * (ord(direct) - 48) ))
-
-
-    def draw(self, screen, focus):
-        screen_pos = vec2d(int(self.pos[0] - focus[0]), int(self.pos[1] - focus[1]))
-        sprite_size = self.sprite.get_size()
-        screen.blit(self.sprite, (int(screen_pos[0] - sprite_size[0]//2), int(screen_pos[1] - sprite_size[1]//2)))
-
-class Buildings():
-
-    def __init__(self):
-        self.walls = []
-
-    def draw(self, screen, focus):
-        for wall in self.walls:
-            wall.draw(screen, focus)
--- utils.py
+++ utils.py
@@ -35,19 +35,32 @@ def update_focus(alt_player_pos, focus):
     if alt_player_pos[1] < 250:
         focus[1] -= 250 - alt_player_pos[1]
 
+class Wall():
+
+    sprite_dict = { '1':"wall", '2':"door", '3':"window"}
+
+    size = 25
+
+    def __init__(self, pos, wall_type, direct):    
+        self.pos = pos
+        self.dir = direct
+        self.type = wall_type
+
+        self.sprite = pygame.image.load("assets/obsticles/" + self.sprite_dict[wall_type] + ".png")
+        if not direct == "0":
+            self.sprite = pygame.transform.rotate(self.sprite, int( 90 * (ord(direct) - 48) ))
+
+
+    def draw(self, screen, focus):
+        screen_pos = vec2d(int(self.pos[0] - focus[0]), int(self.pos[1] - focus[1]))
+        sprite_size = self.sprite.get_size()
+        screen.blit(self.sprite, (int(screen_pos[0] - sprite_size[0]//2), int(screen_pos[1] - sprite_size[1]//2)))
 
 class Map_cell():
     pos = vec2d(0 ,0)
     static_content = null
 
-class Map():
     def __init__(self):
-        pass
-
-    def define_wall(self, wall):
-        pass
-
-    def define_item(self, item):
         pass
 
     def map_coords_to_pixels(self, coords):
@@ -55,3 +68,32 @@ class Map():
 
     def pixels_to_map_coords(self, coords):
         return vec2d(coords[0]//30, coords[1]//30)
+
+class Map():
+
+    def __init__(self):
+        map_file = open('assets/map.csv', 'r')
+        map_line = map_file.readline()
+        coords = [0, 0]
+        while not map_line == '':
+            cells = map_line.split(";")
+            for cell in cells:
+                cell_info = cell.split("/")
+                if not cell_info[0] == "0":
+                    if cell_info[1] == "0/n":
+                        cell_info[1] = "0"
+                    buildings.walls.append(Wall(vec2d(coords[0] * 30 + 15, coords[1] * 30 + 15), cell_info[0], cell_info[1]))
+                coords[0] += 1
+
+
+            coords[1] += 1
+            coords[0] = 0
+            map_line = map_file.readline()
+
+    def define_wall(self, wall):
+        pass
+
+    def define_item(self, item):
+        pass
+
+    
